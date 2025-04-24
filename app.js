776fa70cdcf6966f77d2c3ea4a1047d9084208e2
riddle shi <154901060+lordshit@users.noreply.github.com>
2025-04-24 20:52:06 +0600

    Added clicking sound-effects

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const ratingRoutes = require('./routes/rating');
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(express.static('public')); // Serve static files from the "public" folder
 
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.session());
